8bfda6e3b2c06194736fff75487201109783dbcf
Davis Engeler <davisengeler+github@gmail.com>
2022-04-23 13:57:09 -0400

    Switch to vainID

--- api/lytnit_shorten.py
+++ api/lytnit_shorten.py
@@ -1,11 +1,10 @@
 from urllib.parse import urlparse
-from math import gcd
-import math
-import hashlib
 import boto3
 import datetime
 import uuid
 import json
+
+import vainID
 
 seed = "lytnit"
 
@@ -18,22 +17,6 @@ def track_event(event_type, event_data):
         print(s3_client.put_object(Body=json.dumps(event_data), Bucket=dest_bucket, Key=dest_key))
     except Exception as e:
         print(e)
-
-def to_base(n): 
-    chars="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    base = len(chars)
-    if not n: return "0"
-    return to_base(n//base).lstrip("0") + chars[int(n%base)]
-
-def create_generator(capacity, seed):
-    hashed_seed = int(hashlib.sha256(seed.encode('utf-8')).hexdigest(), 16)
-    generator = hashed_seed % math.floor(capacity*.8)  # Starting point
-    while gcd(generator,capacity)!=1 and generator < capacity:  # We need a generator that's coprime to the capacity.
-        generator += 1  # There may be a smarter way to find one, but I just increment by 1 each time until I find one.
-    if gcd(generator,capacity)!=1:
-        # Raise an error if one can't be found. 
-        raise ValueError("Could not find a 'generator' that is relatively prime to the capacity ({capacity})")
-    return generator
     
 def get_iteration(seed):
     iterators = boto3.resource('dynamodb').Table('lytn.it_iterators')
@@ -44,26 +27,6 @@ def get_iteration(seed):
         ReturnValues="UPDATED_NEW"
     )
     return response['Attributes']['iteration']
-
-def generate_id(seed):
-    # Determine required values
-    iteration = get_iteration(seed)
-    try: # Determine the number of digits required for this iteration value
-        required_digits = math.ceil(math.log(iteration+1)/math.log(62))
-    except:
-        required_digits = 1
-    try: # Determine the capacity of the previous number of digits (how many are 'unavailable')
-        unavailable = math.ceil(math.pow(62, required_digits-1))
-        if unavailable == 1:
-            unavailable = 0
-    except:
-        unavailable = 0
-    capacity = max(62, math.ceil(math.pow(62, required_digits)) - unavailable)
-    generator = create_generator(capacity, seed)
-    # Calcuate the next id to generate and convert it to a base 62 alphanumeric string
-    calc = ((iteration-unavailable)*generator) % capacity + unavailable
-    id = to_base(calc)
-    return id
 
 def has_conflict(id):
     forwards = boto3.resource('dynamodb').Table('lytn.it_forwards')
@@ -92,7 +55,7 @@ def lambda_handler(event, context):
     clean_dest = f"http://{dest}" if (not meets_url_requirements(dest)) else dest
     if meets_url_requirements(clean_dest):
         while True:
-            id = generate_id(seed)
+            id = vainID.generate_id(seed)
             if not has_conflict(id):
                 print(f"There was no conflict with {id}")
                 break
---
+++ build_api.sh
@@ -0,0 +1,3 @@
+cd api
+zip -r ../builds/lytnit_api__build_$(date +%s).zip .
+cd ..
